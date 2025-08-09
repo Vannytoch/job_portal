2c9f264c1ef85cf02d2b4649a0d36e87a66c7189
Vannytoch <vannytoch996@gmail.com>
2025-08-09 19:53:42 +0700

    Use env vars for SECRET_KEY/DEBUG and add WhiteNoise static settings

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -19,13 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+qdqvu&e8f1ficeh3egacqrbmqwf+ljh1znrm_ekh++q5int*3'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-default')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG from env (string): on Render set DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # add WhiteNoise here
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
